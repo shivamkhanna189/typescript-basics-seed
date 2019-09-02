f6d30420868ffd86a09cb0ab01a2855c85537ee2
Shivam Khanna <shivam.khanna@finoit.co.in>
2019-09-02 16:15:26 +0530

    ()=> arrowFunction

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
 console.log('Hello TypeScript!');
+
+const pizzas = [
+  { name: "peperroni", toppings: ["peproni"] }
+]
+
+const mappedPizzas = pizzas.map(data => data.name.toUpperCase())
+console.log(pizzas, mappedPizzas);
+
+const pizza = {
+  name: "Blazing Inferno",
+  getName: () => pizza.name
+}
+
+console.log(pizza.getName());
+
+
